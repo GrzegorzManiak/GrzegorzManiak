dedad6b192148001ecb2575eb37e6263ce2fc17b
Grzegorz Maniak <git@grzegorz.ie>
2023-05-07 18:01:10 +0100

    Reset back to normal

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,27 +1,4 @@
-import Konva from 'konva';
-
-export interface PhysicsObject {
-    x: number;
-    y: number;
-    
-    velocity: Vector2D;
-    acceleration: Vector2D;
-
-    mass: number;
-    elasticity: number;
-    friction: number;
-    fixed: boolean;
-    shape: Konva.Shape;
-}
-
-export interface PhysicsEnvironment {
-    stage: Konva.Stage;
-    layer: Konva.Layer;
-    objects: Array<PhysicsObject>;
-    coefficient_of_restitution: number;
-}
-
-export interface Vector2D {
+export interface Coords {
     x: number;
     y: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import main_text from './core/main_text';
-import scroll_handler from './core/scroll_handler';
-import Physics from './physics/engine';
-import build_stage from './physics/stage';
+// // a is 5 here
+// function x($t){ return 5 * $t * cos($t); }
+// function y($t){ return 5 * $t * sin($t); }
 
-export const NAV_MAIN_TEXT_SELECTOR = '#nav-main-text';
+// for ($t = 0; $t < 50; $t += 0.01) {
+//     $xyPoint = array(x($t), y($t));
+//     // draw it
+// }
 
-// -- Main entry point
-scroll_handler.getInstance();
-main_text();
+import { Coords } from './index.d';
 
-// -- Physics
-const env = build_stage(),
-    engine = new Physics(env);
-
-engine.debug(true);
-engine.start();
+function spiral(
+    size: Coords,
+    center: Coords,
+    delta: number,
+    angle: number,
+    old_point: Coords
+): Coords {
+    const x = old_point.x + delta * Math.cos(angle);
+    const y = old_point.y + delta * Math.sin(angle);
+    return { x, y };
+}
